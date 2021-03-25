e21077f4ef037b451f7cef622eb2da1738022e88
Wellington Garcia <wellington.garcia@wisereducacao.com>
2021-03-24 21:32:05 -0300

    configurando eslint prettier editorconfig

--- node/project2/src/routes.ts
+++ node/project2/src/routes.ts
@@ -1,8 +1,9 @@
-import  { Request, Response} from "express"
-import CreateCourseService from "./CreateCourseService"
+import { Request, Response } from "express";
+
+import CreateCourseService from "./CreateCourseService";
 
 export function createCrouse(request: Request, response: Response) {
-    CreateCourseService.execute({course: "Node JS", time: 10, educator: "Wel"})
+  CreateCourseService.execute({ course: "Node JS", time: 10, educator: "Wel" });
 
-    return response.send();
+  return response.send();
 }
