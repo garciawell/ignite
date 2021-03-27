bbbb5d4d8830770f5fc4e01649b9daffefa7d064
Wellington Garcia <wellington.garcia@wisereducacao.com>
2021-03-27 12:41:05 -0300

    Create router categories

--- node/project2/src/routes.ts
+++
@@ -1,9 +0,0 @@
-import { Request, Response } from "express";
-
-import CreateCourseService from "./CreateCourseService";
-
-export function createCrouse(request: Request, response: Response) {
-  CreateCourseService.execute({ course: "Node JS", time: 10, educator: "Wel" });
-
-  return response.send();
-}
