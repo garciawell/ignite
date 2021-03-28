8335d5f0cd39149523214d7104effb9bc18d2c40
Wellington Garcia <wellington.garcia@wisereducacao.com>
2021-03-28 17:38:33 -0300

    specification

---
+++ node/project2/src/modules/cars/repositories/SpecificationRepository.ts
@@ -0,0 +1,35 @@
+import { Specification } from "../models/Specification";
+import {
+  ISpecificationRepository,
+  ICreateSpecificationDTO,
+} from "./ISpecificationRepository";
+
+class SpecificationRepository implements ISpecificationRepository {
+  private specifications: Specification[];
+
+  constructor() {
+    this.specifications = [];
+  }
+
+  create({ name, description }: ICreateSpecificationDTO): void {
+    const specification = new Specification();
+
+    Object.assign(specification, {
+      name,
+      description,
+      created_at: new Date(),
+    });
+
+    this.specifications.push(specification);
+  }
+
+  findByName({ name }: { name: string }): Specification {
+    const Specification = this.specifications.find(
+      (item) => item.name === name
+    );
+
+    return Specification;
+  }
+}
+
+export { SpecificationRepository };
--- node/project2/src/modules/cars/services/CreateSpecificationService.ts
+++ node/project2/src/modules/cars/services/CreateSpecificationService.ts
@@ -1,6 +1,23 @@
+import { ISpecificationRepository } from "../repositories/ISpecificationRepository";
+
+interface IRequest {
+  name: string;
+  description: string;
+}
 class CreateSpecificationService {
-  execute() {
-    //
+  constructor(private specificationRepository: ISpecificationRepository) {}
+
+  execute({ description, name }: IRequest): void {
+    const alreadyExists = this.specificationRepository.findByName({ name });
+
+    if (alreadyExists) {
+      throw new Error("Alreadt Exist");
+    }
+
+    this.specificationRepository.create({
+      name,
+      description,
+    });
   }
 }
 
