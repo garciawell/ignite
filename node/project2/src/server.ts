f94a764f4b96ac17648ea6639dc91b95a4f7aab9
Wellington Garcia <wellington.garcia@wisereducacao.com>
2021-03-29 23:02:13 -0300

    add swagger project and remove file

--- node/project2/src/server.ts
+++ node/project2/src/server.ts
@@ -1,9 +1,12 @@
 import express from "express";
+import swaggerUi from "swagger-ui-express";
 
 import { router } from "./routes";
+import swaggerOptions from "./swagger.json";
 
 const app = express();
 app.use(express.json());
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerOptions));
 app.use(router);
 
 app.listen(3333, () => console.log("Server is running"));
