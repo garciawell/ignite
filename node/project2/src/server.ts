2ff08a21b30aa9e34cfd69a7d222b9045206e1b2
Wellington Garcia <wellington.garcia@wisereducacao.com>
2021-04-04 23:07:29 -0300

    Handle Errors

--- node/project2/src/server.ts
+++ node/project2/src/server.ts
@@ -1,9 +1,11 @@
 import * as dotenv from "dotenv";
-import express from "express";
+import express, { NextFunction, Request, Response } from "express";
+import "express-async-errors";
 import swaggerUi from "swagger-ui-express";
 
 import "./database";
 import "./shared/container";
+import { AppError } from "./errors/AppError";
 import { router } from "./routes";
 import swaggerOptions from "./swagger.json";
 
@@ -13,5 +15,18 @@ const app = express();
 app.use(express.json());
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerOptions));
 app.use(router);
+
+app.use((err: Error, req: Request, resp: Response, next: NextFunction) => {
+  if (err instanceof AppError) {
+    return resp.status(err.statusCode).json({
+      message: err.message,
+    });
+  }
+
+  return resp.status(500).json({
+    status: "error",
+    message: `Internal server error - ${err.message}`,
+  });
+});
 
 app.listen(3333, () => console.log("Server is running"));
