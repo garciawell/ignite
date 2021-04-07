e949e2c404c4f49debeb8c172098c7efd45afb74
Wellington Garcia <wellington.garcia@wisereducacao.com>
2021-04-06 23:27:23 -0300

    Seed admin

--- node/project2/src/shared/infra/http/server.ts
+++ node/project2/src/shared/infra/http/server.ts
@@ -1,17 +1,19 @@
 import * as dotenv from "dotenv";
+import "reflect-metadata";
 import express, { NextFunction, Request, Response } from "express";
 import "express-async-errors";
 import swaggerUi from "swagger-ui-express";
 
-import "@shared/infra/typeorm";
 import "@shared/container";
 import { AppError } from "@shared/errors/AppError";
 import { router } from "@shared/infra/http/routes";
+import createConnection from "@shared/infra/typeorm";
 
 import swaggerOptions from "../../../swagger.json";
 
 dotenv.config({ path: `${__dirname}/.env` });
 
+createConnection();
 const app = express();
 app.use(express.json());
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerOptions));
