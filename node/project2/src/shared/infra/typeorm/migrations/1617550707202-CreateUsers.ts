982b41da65c1b1cdf565a39ba7070716d4d0751a
Wellington Garcia <wellington.garcia@wisereducacao.com>
2021-04-08 20:57:18 -0300

    migration rentals

--- node/project2/src/shared/infra/typeorm/migrations/1617550707202-CreateUsers.ts
+++ node/project2/src/shared/infra/typeorm/migrations/1617550707202-CreateUsers.ts
@@ -9,6 +9,7 @@ export class CreateUsers1617550707202 implements MigrationInterface {
           {
             name: "id",
             type: "uuid",
+            isPrimary: true,
           },
           {
             name: "name",
