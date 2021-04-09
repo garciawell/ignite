c0ad2c0d63405943290ead797f8a8c224aa516d7
Wellington Garcia <wellington.garcia@wisereducacao.com>
2021-04-08 21:37:45 -0300

    test rentals

---
+++ node/project2/src/modules/rentals/repositories/fake/RentalsRepositoryFake.ts
@@ -0,0 +1,33 @@
+import { ICreateRentalDTO } from "@modules/rentals/dtos/ICreateRentalDTO";
+import { Rental } from "@modules/rentals/infra/typeorm/entities/Rental";
+
+import { IRentalsRepository } from "../IRentalsRepository";
+
+class RentalsRepositoryFake implements IRentalsRepository {
+  rentals: Rental[] = [];
+
+  async findOpenRentalByCar(car_id: string): Promise<Rental> {
+    return this.rentals.find(
+      (rental) => rental.car_id === car_id && !rental.end_date
+    );
+  }
+  async findOpenRentalByUser(user_id: string): Promise<Rental> {
+    return this.rentals.find(
+      (rental) => rental.user_id === user_id && !rental.end_date
+    );
+  }
+
+  async create(data: ICreateRentalDTO): Promise<Rental> {
+    const rental = new Rental();
+
+    Object.assign(rental, {
+      ...data,
+      start_date: new Date(),
+    });
+    this.rentals.push(rental);
+
+    return rental;
+  }
+}
+
+export { RentalsRepositoryFake };
---
+++ node/project2/src/modules/rentals/useCases/createRental/__tests__/CreateRentalUseCase.spec.ts
@@ -0,0 +1,55 @@
+import { RentalsRepositoryFake } from "@modules/rentals/repositories/fake/RentalsRepositoryFake";
+import { AppError } from "@shared/errors/AppError";
+
+import { CreateRentalUseCase } from "../CreateRentalUseCase";
+
+let rentalsRepository: RentalsRepositoryFake;
+let createRentalUseCase: CreateRentalUseCase;
+describe("Create Rental", () => {
+  beforeEach(() => {
+    rentalsRepository = new RentalsRepositoryFake();
+    createRentalUseCase = new CreateRentalUseCase(rentalsRepository);
+  });
+
+  it("should be able to create a new rental", async () => {
+    const rental = await createRentalUseCase.execute({
+      car_id: "123123145",
+      expected_return_date: new Date(),
+      user_id: "123456",
+    });
+
+    expect(rental).toHaveProperty("id");
+    expect(rental).toHaveProperty("start_date");
+  });
+
+  it("should not be able to create a new rental if there is another open to the same user", async () => {
+    expect(async () => {
+      await createRentalUseCase.execute({
+        car_id: "123123145",
+        expected_return_date: new Date(),
+        user_id: "123456d",
+      });
+
+      await createRentalUseCase.execute({
+        car_id: "123123145",
+        expected_return_date: new Date(),
+        user_id: "123456",
+      });
+    }).rejects.toBeInstanceOf(AppError);
+  });
+  it("should not be able to create a new rental if there is another open to the same car", async () => {
+    expect(async () => {
+      await createRentalUseCase.execute({
+        car_id: "qqq",
+        expected_return_date: new Date(),
+        user_id: "321",
+      });
+
+      await createRentalUseCase.execute({
+        car_id: "qqq",
+        expected_return_date: new Date(),
+        user_id: "3212",
+      });
+    }).rejects.toBeInstanceOf(AppError);
+  });
+});
