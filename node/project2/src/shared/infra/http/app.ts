8cb9202088c22be9f1fb98e087eb862057556448
Wellington Garcia <wellington.garcia@wisereducacao.com>
2021-04-09 21:41:26 -0300

    test integration - supertest controllers

---
+++ node/project2/src/shared/infra/http/app.ts
@@ -0,0 +1,34 @@
+import * as dotenv from "dotenv";
+import "reflect-metadata";
+import express, { NextFunction, Request, Response } from "express";
+import "express-async-errors";
+import swaggerUi from "swagger-ui-express";
+
+import "@shared/container";
+import { AppError } from "@shared/errors/AppError";
+import { router } from "@shared/infra/http/routes";
+import createConnection from "@shared/infra/typeorm";
+
+import swaggerOptions from "../../../swagger.json";
+
+dotenv.config({ path: `${__dirname}/.env` });
+
+createConnection();
+const app = express();
+app.use(express.json());
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerOptions));
+app.use(router);
+
+app.use((err: Error, req: Request, resp: Response, next: NextFunction) => {
+  if (err instanceof AppError) {
+    return resp.status(err.statusCode).json({
+      message: err.message,
+    });
+  }
+
+  return resp.status(500).json({
+    status: "error",
+    message: `Internal server error - ${err.message}`,
+  });
+});
+export { app };
--- node/project2/src/shared/infra/http/server.ts
+++ node/project2/src/shared/infra/http/server.ts
@@ -1,35 +1,3 @@
-import * as dotenv from "dotenv";
-import "reflect-metadata";
-import express, { NextFunction, Request, Response } from "express";
-import "express-async-errors";
-import swaggerUi from "swagger-ui-express";
-
-import "@shared/container";
-import { AppError } from "@shared/errors/AppError";
-import { router } from "@shared/infra/http/routes";
-import createConnection from "@shared/infra/typeorm";
-
-import swaggerOptions from "../../../swagger.json";
-
-dotenv.config({ path: `${__dirname}/.env` });
-
-createConnection();
-const app = express();
-app.use(express.json());
-app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerOptions));
-app.use(router);
-
-app.use((err: Error, req: Request, resp: Response, next: NextFunction) => {
-  if (err instanceof AppError) {
-    return resp.status(err.statusCode).json({
-      message: err.message,
-    });
-  }
-
-  return resp.status(500).json({
-    status: "error",
-    message: `Internal server error - ${err.message}`,
-  });
-});
+import { app } from "./app";
 
 app.listen(3333, () => console.log("Server is running"));
